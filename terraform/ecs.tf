5bdd8b728b216640e907b6e26fbaec0b14176a09
iradics <75294569+iradics1974@users.noreply.github.com>
2026-01-27 19:44:03 +0100

    fix: update ECS task health check command to use port 80

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -130,7 +130,7 @@ resource "aws_ecs_task_definition" "this" {
       ]
 
       healthCheck = {
-        command     = ["CMD-SHELL", "wget -qO- http://localhost:8080 || exit 1"]
+        command     = ["CMD-SHELL", "wget -qO- http://localhost:80 || exit 1"]
         interval    = 30
         timeout     = 5
         retries     = 3
